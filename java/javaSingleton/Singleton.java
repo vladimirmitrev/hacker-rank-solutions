2821155d1f55d3e396e5525d165930b154527732
Vladimir Mitrev <orionsbelt1017@gmail.com>
2025-01-25 20:02:03 +0200

    javaSigleton

---
+++ java/javaSingleton/Singleton.java
@@ -0,0 +1,30 @@
+package javaSingleton;
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+
+class Singleton {
+    // private static Singleton instance;
+    public String str;
+
+    private Singleton() {
+    }
+
+    public static Singleton getSingleInstance() {
+        // if(instance == null) {
+        //     synchronized(Singleton.class) {
+        //         if(instance == null) {
+        //             instance = new Singleton();
+        //         }
+        //     }
+        // }
+        return new Singleton();
+    }
+}
+
