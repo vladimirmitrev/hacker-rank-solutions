6cf52e82a0ccd98cb60730465ef6aa36683896dc
Vladimir Mitrev <orionsbelt1017@gmail.com>
2025-01-26 20:20:06 +0200

    add javaSHA256

---
+++ java/javaSHA256/Solution.java
@@ -0,0 +1,39 @@
+package javaSHA256;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String plainText = scanner.nextLine();
+        String encrypted = encryptWithMD5(plainText);
+
+        System.out.println(encrypted);
+    }
+
+    private static String encryptWithMD5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(input.getBytes());
+
+            byte[] digest = md.digest();
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : digest) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
