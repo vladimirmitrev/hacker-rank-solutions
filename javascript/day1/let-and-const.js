c13a9ba6ce021b85c302ea1384290d327b375150
Vladimir Mitrev <orionsbelt1017@gmail.com>
2025-01-28 11:35:34 +0200

    add javascript/day1/let-and-const

---
+++ javascript/day1/let-and-const.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    
+    // const r = readLine();
+    const r = 2.6;
+    
+    const PI = Math.PI * r * r;
+    const per = 2 * Math.PI * r;
+    // Print the area of the circle:
+    console.log(PI);
+    // Print the perimeter of the circle:
+    console.log(per);
+    try {    
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
+
+main();
